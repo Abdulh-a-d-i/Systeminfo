d9596d54ccca2699a396bca7a8a70098c8e5a6af
Abdul Hadi <abdulhadizaeem7@gmail.com>
2025-07-27 23:37:46 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,1 @@
-#!/bin/bash
 
-echo "Updating system packages..."
-sudo apt update
-
-echo "Installing lshw if not already installed..."
-sudo apt install -y lshw
-
-echo "Installing Python dependencies..."
-pip install --user psutil
-
-echo "Running system information script..."
-python3 script.py
-
-if [ -f "system_info.json" ]; then
-    echo "✅ system_info.json created successfully!"
-else
-    echo "❌ Failed to create system_info.json."
-fi
